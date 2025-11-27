4a717363a561648e7b1e8a2fe93cac514eb8188d
RMX_NUL <vanya130211@gmail.com>
2025-11-27 21:02:43 +0200

    lil

--- frontend/app/router_user.py
+++ frontend/app/router_user.py
@@ -17,3 +17,19 @@ async def index(request: Request):
     response = templates.TemplateResponse('pages/index.html', context=context)
 
     return response
+
+
+
+@router_user.get("/register")
+@router_user.post("/register")
+async def user_register(request: Request):
+    print(request.method, 8888888888888)
+    context = {
+        "request": request,
+        "title": "Головна сторінка сайту",
+        "user": {}
+    }
+
+    response = templates.TemplateResponse('pages/register.html', context=context)
+
+    return response
